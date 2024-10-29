5224f5869f06981de662d0c45e1e1ee23483dcc9
Daniela <nam991019@gmail.com>
2024-10-28 23:49:11 -0500

    adivina el numero

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 //añadir numero secreto//
-const numeroSecreto = Math.floor(Math.random() * 20) + 1;
+let numeroSecreto = 28;
 
 //Variable que controla si el numero es adivinado//
-let adivina = false; 
+let adivina = parseInt(prompt("ingresa un numero de (1 al 50)"));
 
-//bucle del juego//
-while (!adivina) {
-    //pide que ingrese su suposicion//
-    let suposicion = parseInt(prompt("Adivina el numero entre el 1 al 20:"))
-
-    //valida la susposicion del numero
-    if (isNaN(suposicion)) {
-        alert("Por favor, ingresa un numero valido.");
-        continue; //vuelve al bucle del juego//
-    }
-}
 
 //compara la suposicion//
-if (suposicion < numeroSecreto) {
-    alert("Tu suposiciones menor que el número secreto. Intenta nuevamente.");
-} else if(suposicion > numeroSecreto) {
+if (adivina === numeroSecreto) {
+     alert("¡Felicidades!! Has adivinado el número secreto:" + numeroSecreto);
+} else if(adivina > numeroSecreto) {
     alert("Tu suposicion es mayor que el número secreto. Intenta nuevamente.");
 } else {
-    alert("¡Felicidades!! Has adivinado el número secreto:" + numeroSecreto);
+    alert("Tu suposiciones menor que el número secreto. Intenta nuevamente.");
     adivinado = true; //salir de bucle
 }
